089f53d865c0e1cda6b10421aa32adaaf5114ff7
Felipe Fragoso <62634029+FeFragoso@users.noreply.github.com>
2022-01-24 15:29:56 +0000

    Adição de páginas html

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
+# Dependências
 import pandas as pd
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__) 
+# Inicializador do Flask
+app = Flask(__name__, template_folder='') 
 
-@app.route("/")
+tabela = pd.read_csv("BitCoin.csv")
+
+# Páginas do Site
+@app.route('/')
 def home():
-  tabela = pd.read_csv("BitCoin.csv")
+  return render_template('home.html')
 
+@app.route('/mediavolume')
+def mediaVolume():
   mediaVolume = tabela["btc_trade_volume"].mean()
 
   output1 = {'mediaVolume': mediaVolume}
 
   return output1
 
-app.run(host = '0.0.0.0')
+# Rota
+if __name__ == "__main__":
+  app.run(host = '0.0.0.0')
